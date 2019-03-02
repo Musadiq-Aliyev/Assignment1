5999e88065a0c08afea33cb3aa25bebc0685f048
Musadiq Aliyev <musadiq.aliyev@gmail.com>
2019-03-02 18:59:20 +0400

    Thirdtask

---
+++ 3fwdstdout/third.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/param.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <time.h>
+#include <fcntl.h>
+
+
+void fwdstdout(char *path){
+
+  int out = open(path, O_RDWR|O_CREAT|O_APPEND, 0600);
+
+  int save_out = dup(fileno(stdout));
+
+
+    if (-1 == dup2(out, fileno(stdout))) { perror("cannot redirect stdout"); }
+
+
+
+
+
+}
